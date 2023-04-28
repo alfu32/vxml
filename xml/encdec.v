10dc8b97dde7926baab46d22fd5f978db1d4ee6c
alfu32 <ioan.alferaru@gmail.com>
2023-04-29 00:08:46 +0200

    feat(decode)

--- xml/encdec.v
+++ xml/encdec.v
@@ -16,20 +16,21 @@ fn get_int(data string, field string) int {
 	return get_string(data, field).int()
 }
 
-pub fn decode[T](data string) T {
+pub fn decode[T](data string) []T {
 	mut docxml:=parse(data)
 	classname:=(T.name)
 	nodes:=docxml.get_elements_by_tag_name(classname)
-	mut result := T{}
+	mut result := []T{}
 	// compile-time `for` loop
 	// T.fields gives an array of a field metadata type
-	$for field in T.fields {
-		$if field.typ is string {
-			// $(string_expr) produces an identifier
-			result.$(field.name) = get_string(data, field.name)
-		} $else $if field.typ is int {
-			result.$(field.name) = get_int(data, field.name)
+	for node in nodes {
+		mut inst := $(T.name){}
+		$for field in T.fields {
+
+			name:=node.get_element_by_tag_name('${field.name}') or { panic(err) }
+			inst.$(field.name)=name.get_text()
 		}
+		result<<inst
 	}
 	return result
 }
